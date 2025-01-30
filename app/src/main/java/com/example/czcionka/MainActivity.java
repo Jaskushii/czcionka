b24c5643f10b47054e9bd5ad84a8c0307244a8b4
WJ <wojcioch928@gmail.com>
2025-01-30 09:11:18 +0100

    update

--- app/src/main/java/com/example/czcionka/MainActivity.java
+++ app/src/main/java/com/example/czcionka/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.czcionka;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,45 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+ private int index = 0;
+ private String[] tekst = {"Dzień dobry", "Good morning", "Buenos dias"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
+        TextView textView = findViewById(R.id.textView3);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        Button button = findViewById(R.id.button);
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setTextSize(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        button.setOnClickListener(v -> {
+            index = (index + 1) % tekst.length;
+            textView.setText(tekst[index]);
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
